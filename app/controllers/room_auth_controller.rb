3b58c2b4ef05cd3b73cd258f8de51e514e23ccfd
doodleman218 <dcwolff218@gmail.com>
2024-01-19 13:09:41 -0600

    testing code to send out all users for waiting room. commented out for now

--- app/controllers/room_auth_controller.rb
+++ app/controllers/room_auth_controller.rb
@@ -12,6 +12,9 @@ class RoomAuthController < ApplicationController
       payload = {room_id: room.id}
       token = JWT.encode(payload, "hmac_secret", 'HS256')
       render json: { room: RoomSerializer.new(room), jwt: token, user: user }, status: :accepted
+      # code below does send off what i need when a new user is made, but need to clean up how front proccess this
+      # all_users = room.users.all
+      # UsersChannel.broadcast_to room, {allUsers: all_users}
     else
       render json: { error: 'Invalid roomname or password' }, status: :unauthorized
     end
