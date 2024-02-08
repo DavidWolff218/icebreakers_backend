52a22f1a531a9099c480ecfd50c07dccadd0a49f
doodleman218 <dcwolff218@gmail.com>
2024-02-08 11:08:06 -0600

    added logic for handling duplicate player names

--- app/controllers/room_auth_controller.rb
+++ app/controllers/room_auth_controller.rb
@@ -7,6 +7,10 @@ class RoomAuthController < ApplicationController
   def create
     room = Room.find_by(room_name: room_params[:room_name])
     if room && room.authenticate(room_params[:password])
+      if room.users.exists?(username: room_params[:username])
+        render json: {error: "That Player Name is already being used, please pick a new one"}, status: :conflict
+        return
+      end
       user = User.create({"username" => room_params[:username], :is_active => true})
       join = UserRoom.create({"user_id" => user.id, "room_id" => room.id})
       payload = {room_id: room.id}
