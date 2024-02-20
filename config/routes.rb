f05c4079d76d7f8e99dc6b75a44669a78ebe61d9
doodleman218 <dcwolff218@gmail.com>
2024-02-20 14:22:41 -0600

    cleaned up 'foo' routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  patch '/users/select/foo', to: 'users#select'
-  patch '/users/voting/foo', to: 'users#voting_select'
-  patch '/users/start/foo', to: 'users#start'
-  patch '/users/voting_timer/foo', to: 'users#voting_timer_select'
+  patch '/users/select', to: 'users#select'
+  patch '/users/start', to: 'users#start'
+  # patch '/users/voting/foo', to: 'users#voting_select'
+  # patch '/users/voting_timer/foo', to: 'users#voting_timer_select'
 
   get '/users/by_room/:room_id', to: 'users#by_room'
   get '/users/midgame/:room_id', to: 'users#midgame'
