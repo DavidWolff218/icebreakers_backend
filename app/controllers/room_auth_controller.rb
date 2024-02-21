04b06d8a1c09ebfaef16898200c5515b62527ae6
doodleman218 <dcwolff218@gmail.com>
2024-02-21 10:47:21 -0600

    cleaned up room_auth controller, removed new user func

--- app/controllers/room_auth_controller.rb
+++ app/controllers/room_auth_controller.rb
@@ -1,9 +1,5 @@
 class RoomAuthController < ApplicationController 
 
-  def new
-    user = User.new
-  end
-  
   def create
     room = Room.find_by(room_name: room_params[:room_name])
     if room && room.authenticate(room_params[:password])
@@ -22,17 +18,13 @@ class RoomAuthController < ApplicationController
     else
       render json: { error: 'Invalid Room Name or Password' }, status: :unauthorized
     end
-
-    
   end
-
 
   private
 
   def room_params
     params.require(:room).permit(:room_name, :password, :username)
   end
-
 
 end
 
